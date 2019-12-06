474fc2e76545f3989de642c07c298ae23a4e2cab
simoncdesmet <simoncdesmet@gmail.com>
2019-12-06 12:46:54 +0100

    S3: created create-shoppinglist component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateShoppinglistComponent } from './shoppinglist/create-shoppinglist/create-shoppinglist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateShoppinglistComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shoppinglist/create-shoppinglist/create-shoppinglist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-shoppinglist',
+  templateUrl: './create-shoppinglist.component.html',
+  styleUrls: ['./create-shoppinglist.component.scss']
+})
+export class CreateShoppinglistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
