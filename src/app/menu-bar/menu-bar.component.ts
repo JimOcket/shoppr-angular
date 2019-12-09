7ae4ea9893517aac74f8e24e8a24ca9d6bfcf8c1
KinifHugo <hugokinif@gmx.fr>
2019-12-09 13:25:33 +0100

    DEMO-Add logOut button for demo purpose

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-menu-bar',
@@ -7,7 +8,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class MenuBarComponent implements OnInit {
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   private loginStatus: string;
@@ -25,5 +26,10 @@ export class MenuBarComponent implements OnInit {
 
   ngOnInit() {
     this.loginStatus =  MenuBarComponent.getUser();
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.router.navigateByUrl(``).then(r => r);
   }
 }
