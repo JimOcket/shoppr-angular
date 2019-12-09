614ba81b5b0cc46ec48f3c461ffb7d9920d76581
KinifHugo <hugokinif@gmx.fr>
2019-12-09 11:49:11 +0100

    S2-when not connected redirect to authentication when connected redirect to create shopping list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,12 +9,8 @@ import {AppRedirect} from './shared/AppRedirect';
 const routes: Routes = [
   {path: 'authentication', component: AuthenticationComponent},
   {path: '', pathMatch: 'full', redirectTo: `${AppRedirect.getDefaultPage()}`},
-  {path: 'home/:email', component: AuthenticationComponent}
-  // todo redirect to user's homepage
-  {path: '', redirectTo: 'create-shoppinglist', pathMatch: 'full'},
   {path: 'create-shoppinglist', component: CreateShoppinglistComponent},
   {path: 'shopping-list-detail/:id', component: ShoppingListDetailComponent},
-  {path: 'authentication', component: AuthenticationComponent}
 ];
 
 @NgModule({
--- src/app/shared/AppRedirect.ts
+++ src/app/shared/AppRedirect.ts
@@ -4,7 +4,8 @@ export class AppRedirect {
   static getDefaultPage() {
     if (localStorage.getItem('currentUser') !== undefined) {
       const user: ShopprAuthentication = JSON.parse(localStorage.getItem('currentUser'));
-      return `home/${user.user.email}`;
+      // return `home/${user.user.email}`;
+      return 'create-shoppinglist';
     } else {
       return 'authentication';
     }
