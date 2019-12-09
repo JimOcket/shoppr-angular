1e7341d0958dcd41ad996cb97532772928c2cb76
simoncdesmet <simoncdesmet@gmail.com>
2019-12-09 13:11:50 +0100

    added _redirects file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateShoppinglistComponent } from './shoppinglist/create-shoppinglist/create-shoppinglist.component';
-import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import {CreateShoppinglistComponent} from './shoppinglist/create-shoppinglist/create-shoppinglist.component';
+import {MenuBarComponent} from './menu-bar/menu-bar.component';
 import {HttpClientModule} from '@angular/common/http';
-import { FooterBarComponent } from './footer-bar/footer-bar.component';
-import { ShoppingListDetailComponent } from './shoppinglist/shopping-list-detail/shopping-list-detail.component';
-import { AuthenticationModule } from './authentication/authentication.module';
+import {FooterBarComponent} from './footer-bar/footer-bar.component';
+import {ShoppingListDetailComponent} from './shoppinglist/shopping-list-detail/shopping-list-detail.component';
+import {AuthenticationModule} from './authentication/authentication.module';
 
 @NgModule({
   declarations: [
