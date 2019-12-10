3043737e855e48179ac0490952ad2a76cdfe5940
Thomas Dardenne <dardenne.thomas@gmail.com>
2019-12-10 12:38:28 +0100

    Message to the user when the mail is invalid or empty

--- src/app/authentication/create-account/create-account.component.ts
+++ src/app/authentication/create-account/create-account.component.ts
@@ -2,9 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../shared/user.service';
 import {AuthenticationService} from '../../shared/authenticationService';
 import {Router} from '@angular/router';
-import {Account} from '../../shared/account';
 import {ShopprAuthentication} from '../../shared/ShopprAuthentication';
 import {MenuBarComponent} from '../../menu-bar/menu-bar.component';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Account} from "../../shared/account";
 
 @Component({
   selector: 'app-create-account',
@@ -12,34 +13,45 @@ import {MenuBarComponent} from '../../menu-bar/menu-bar.component';
   styleUrls: ['./create-account.component.scss']
 })
 export class CreateAccountComponent implements OnInit {
-  personalEmail: string;
-  domainEmail: string;
 
-  private static validateEmail(email: string) {
-    return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email);
-  }
+  accountForm: FormGroup;
+  submitted: false;
 
-  constructor(private userService: UserService, private authService: AuthenticationService,
-              private router: Router, private menuBar: MenuBarComponent) {
+  constructor(private userService: UserService,
+              private authService: AuthenticationService,
+              private router: Router,
+              private menuBar: MenuBarComponent) {
   }
 
   ngOnInit() {
+    this.accountForm = this.createFormGroup();
+  }
+
+  createFormGroup(): FormGroup {
+    return new FormGroup({
+      email: new FormControl('email@domain.com', [Validators.required, Validators.email])
+    });
+  }
+
+  get email() {
+    return this.accountForm.get('email');
   }
 
   save() {
-    const email = this.personalEmail + '@' + this.domainEmail;
-    if (CreateAccountComponent.validateEmail(email)) {
-      const account: Account = new Account();
-      account.email = email;
-      this.userService.createAccount(account).subscribe(getAccount => {
-        this.authService.login(getAccount.email).subscribe(() => {
-          if (localStorage.getItem('currentUser') !== undefined) {
-            const user: ShopprAuthentication = JSON.parse(localStorage.getItem('currentUser'));
-            this.router.navigateByUrl(`create-shoppinglist`).then(r => r);
-            this.menuBar.update();
-          }
-        });
-      });
+    this.submitted = true;
+
+    if (this.accountForm.invalid) {
+      return;
     }
+
+    this.userService.createAccount(this.accountForm.value).subscribe(getAccount => {
+      this.authService.login(getAccount.email).subscribe(() => {
+        if (localStorage.getItem('currentUser') !== undefined) {
+          const user: ShopprAuthentication = JSON.parse(localStorage.getItem('currentUser'));
+          this.router.navigateByUrl(`create-shoppinglist`).then(r => r);
+          this.menuBar.update();
+        }
+      });
+    });
   }
 }
