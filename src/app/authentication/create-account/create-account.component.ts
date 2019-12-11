499b6b55e84fdb7687afb63004dcd3849584defa
KinifHugo <hugokinif@gmx.fr>
2019-12-11 16:03:51 +0100

    s5-Name is now really required

--- src/app/authentication/create-account/create-account.component.ts
+++ src/app/authentication/create-account/create-account.component.ts
@@ -2,9 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../shared/user.service';
 import {AuthenticationService} from '../../shared/authenticationService';
 import {Router} from '@angular/router';
-import {ShopprAuthentication} from '../../shared/ShopprAuthentication';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {ListenerService} from '../../listener.service';
 
 @Component({
   selector: 'app-create-account',
@@ -26,7 +24,7 @@ export class CreateAccountComponent implements OnInit {
 
   createFormGroup(): FormGroup {
     return new FormGroup({
-      email: new FormControl('email@domain.com',
+      email: new FormControl('',
         [Validators.required, Validators.pattern('^[A-Za-z0-9._%+-]+@[A-Za-z0-9._%+-]{2,}[.][A-Za-z]{2,}$')])
     });
   }
